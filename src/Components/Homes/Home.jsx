6ccf34bef7a6803c739daf32fd6df0887b7f77d5
Ram-tyagi <ramtyagi04@gmail.com>
2023-04-06 12:31:44 +0530

    final commint

--- src/Components/Homes/Home.jsx
+++ src/Components/Homes/Home.jsx
@@ -1,119 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import "./Home.scss"
-import axios from 'axios'
-import { BiPlay} from 'react-icons/bi'
-import { AiOutlinePlus } from 'react-icons/ai'
+import React, { useEffect, useState } from "react";
+import "./Home.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { BiPlay } from "react-icons/bi"
+import { AiOutlinePlus } from "react-icons/ai"
 
-const apiKey="0c707e3e634e79283eca2b0b1d25233f"
-const url="https://api.themoviedb.org/3"
-const upcoming="upcoming"
-const imgUrl="https://image.tmdb.org/t/p/original"
-const nowPlaying="now_playing"
-const popular="popular"
-const topRated="top_rated"
+const apiKey = "7e5122f42b3d47b2f9c1deaf4e1d2214";
+const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/original";
+const upcoming = "upcoming";
+const nowPlaying = "now_playing";
+const popular = "popular";
+const topRated = "top_rated";
 
-const Card=({img})=>( 
-  
-     <img className='card' src={img} alt="cover" />
+const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
 
- 
-)
-const Row=({title,arr=[{
-  
-},
+const Row = ({ title, arr = [] }) => (
+    <div className="row">
+        <h2>{title}</h2>
 
+        <div>
+            {arr.map((item, index) => (
+                <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
+            ))}
+        </div>
+    </div>
+);
 
-]})=>( 
-  <div className='row'>
-    <h2>
-      {title}
-    </h2>
-  <div>
-    {
-      arr.map((item,index)=>( 
-        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
-      ))
-    }
-  
-    
-    
-  </div>
-  </div>
-)
 const Home = () => {
-  const [upcomingMovies,setUpcomingMovies]=useState([])
-  const [nowPlayingMovies,setNowPlayingMovies]=useState([])
-  const [popularMovies,setPopularMovies]=useState([])
-  const [topRatedMovies,setTopRatedMovies]=useState([])
-  const [genre,setGenre]=useState([])
-  useEffect(()=>{
-    const fetchUpcoming=async()=>{
-     const {data:{results}}=await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}`)
-     setUpcomingMovies(results)
-    }
-    fetchUpcoming();
-    const fetchnowPlaying=async()=>{
-      const {data:{results}}=await axios.get(`${url}/movie/${nowPlaying}?api_key=${apiKey}`)
-      setNowPlayingMovies(results)
-     }
-     fetchnowPlaying();
-     const fetchpopular=async()=>{
-      const {data:{results}}=await axios.get(`${url}/movie/${popular}?api_key=${apiKey}`)
-      setPopularMovies(results)
-     }
-     fetchpopular();
-     const fetchtopRated=async()=>{
-      const {data:{results}}=await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}`)
-      setTopRatedMovies(results)
-     }
-     fetchtopRated()
-     const getAllGenre=async()=>{
-      const {
-        data:{genres},
+    const [upcomingMovies, setUpcomingMovies] = useState([]);
+    const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [genre, setGenre] = useState([]);
 
-      }=await axios.get(`${url}/genre/movie/list?=${apiKey}`)
-      setGenre(genres)
-     }
-     getAllGenre()
-  },[])
+    useEffect(() => {
+        const fetchUpcoming = async () => {
+            const {
+                data: { results },
+            } = await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}`);
+            setUpcomingMovies(results);
+        };
+        const fetchNowPlaying = async () => {
+            const {
+                data: { results },
+            } = await axios.get(`${url}/movie/${nowPlaying}?api_key=${apiKey}`);
+            setNowPlayingMovies(results);
+        };
+        const fetchPopular = async () => {
+            const {
+                data: { results },
+            } = await axios.get(`${url}/movie/${popular}?api_key=${apiKey}`);
+            setPopularMovies(results);
+        };
+        const fetchTopRated = async () => {
+            const {
+                data: { results },
+            } = await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}`);
+            setTopRatedMovies(results);
+        };
+        const getAllGenre = async () => {
+            const {
+                data: { genres },
+            } = await axios.get(`${url}/genre/movie/list?api_key=${apiKey}`);
+            setGenre(genres);
+            console.log(genres);
+        };
 
-  return (
-   <section className='home'>
-  <div className='banner' style={{
-    backgroundImage: popularMovies[0]? `url(${imgUrl}/${popularMovies[0].poster_path})`: 'none'
-  }} >
-    {
-      popularMovies[0]&&
-      (
-        <h1>{popularMovies[0].original_title}</h1>
-      )
-    }
-    {
-      popularMovies[0]&&
-      (
-        <p>
-      {popularMovies[0].overview}
-     </p>
-      ) 
-    }
-    <button><BiPlay/>Play </button>
-    <button>My List <AiOutlinePlus/> </button>
-  
-    
-  </div>
-  <Row title={"Upcoming movies"} arr={upcomingMovies}/>
-  <Row title={"Now Playing"} arr={nowPlayingMovies}/>
-  <Row title={"Popular"} arr={popularMovies}/>
-  <Row title={"Top Rated"} arr={topRatedMovies}/>
-   
-   <div className='genreBox'>
-    {genre.map((item)=>{
-      <link key={item.id} to={`/genre/${item.id}`}>{item.name}</link>
-    })}
+        getAllGenre();
 
-   </div>
-   </section>
-  )
-}
+        fetchUpcoming();
+        fetchNowPlaying();
+        fetchPopular();
+        fetchTopRated();
+    }, []);
 
-export default Home
+    return (
+        <section className="home">
+            <div
+                className="banner"
+                style={{
+                    backgroundImage: popularMovies[0]
+                        ? `url(${`${imgUrl}/${popularMovies[0].poster_path}`})`
+                        : "rgb(16, 16, 16)",
+                }}
+            >
+                {popularMovies[0] && <h1>{popularMovies[0].original_title}</h1>}
+                {popularMovies[0] && <p>{popularMovies[0].overview}</p>}
+
+                <div>
+                    <button><BiPlay /> Play  </button>
+                    <button>My List <AiOutlinePlus /> </button>
+                </div>
+            </div>
+
+            <Row title={"Upcoming"} arr={upcomingMovies} />
+            <Row title={"Now Playing"} arr={nowPlayingMovies} />
+            <Row title={"Popular"} arr={popularMovies} />
+            <Row title={"Top Rated"} arr={topRatedMovies} />
+
+            <div className="genreBox">
+                {genre.map((item) => (
+                    <Link key={item.id} to={`/genre/${item.id}`}>
+                        {item.name}
+                    </Link>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Home;
